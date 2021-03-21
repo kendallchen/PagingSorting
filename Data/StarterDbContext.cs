d6a4f7364b8fb7ce4ce2fd08bce7d051bcd1e397
support <support@dotnetlead.com>
2021-03-20 21:38:07 -0700

    add project

---
+++ Data/StarterDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Model;
+
+namespace Data
+{
+    public class StarterDbContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+
+        public StarterDbContext(DbContextOptions<StarterDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Paging and Sorting using ASP .Net Core Razor Page, Web API, and Entity Framework
+
+https://dotnetlead.com/net/paging-and-sorting-using-asp-net-core-razor-page-web-api-and-entity-framework/2267/
